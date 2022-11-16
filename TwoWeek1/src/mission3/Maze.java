38dab025c859a644a3632389156c80b5a160c353
JIHYE <jdh324@naver.com>
2022-11-16 16:49:13 +0900

    둘째주 월요일 3번 미션 작성

---
+++ TwoWeek1/src/mission3/Maze.java
@@ -0,0 +1,64 @@
+package mission3;
+
+import java.util.Stack;
+
+public class Maze {
+    public static int[][] myMaze ={
+            {0, 0, 1, 1, 1, 1, 1, 1},
+            {1, 0, 0, 0, 0, 0, 0, 1},
+            {1, 1, 1, 0, 1, 1, 1, 1},
+            {1, 1, 1, 0, 1, 1, 1, 1},
+            {1, 0, 0, 0, 0, 0, 0, 1},
+            {1, 0, 1, 1, 1, 1, 1, 1},
+            {1, 0, 0, 0, 0, 0, 0, 0},
+            {1, 1, 1, 1, 1, 1, 1, 0}
+    };
+
+    static int[] dx = {1, 0, -1, 0};
+    static int[] dy = {0, 1, 0, -1};
+
+    static Stack<Integer> stackX = new Stack<>();
+    static Stack<Integer> stackY = new Stack<>();
+
+    static void DFS(int x, int y) {
+        int xx;
+        int yy;
+        int i;
+        stackX.push(x);
+        stackY.push(y);
+
+        for(i = 0; i < 4; i++) {
+            xx = x + dx[i];
+            yy = y + dy[i];
+            if(xx == (myMaze.length-1) && yy == (myMaze.length-1)) {
+                while (!stackX.isEmpty()) {
+                    System.out.print("(" + stackX.elementAt(0) + "," + stackY.elementAt(0) + ") -> ");
+                    stackX.remove(0);
+                    stackY.remove(0);
+                }
+                System.out.println("(7,7)");
+                return;
+            }
+            if(xx < 0 || yy < 0 || xx > 7 || yy > 7)
+                continue;
+            if(myMaze[xx][yy] == 0) {
+                myMaze[xx][yy] = 2;
+                DFS(xx, yy);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("출구는 (" + (myMaze.length-1) + "," + (myMaze.length-1) + ") 입니다.");
+        for(int i = 0; i < myMaze.length; i++) {
+            for(int j = 0; j < myMaze.length; j++) {
+                if(myMaze[i][j] == 0)
+                    System.out.print(" ");
+                else
+                    System.out.print("*");
+            }
+            System.out.println();
+        }
+        Maze.DFS(0,0);
+    }
+}
